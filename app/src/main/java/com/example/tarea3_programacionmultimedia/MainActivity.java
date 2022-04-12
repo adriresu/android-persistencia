ba49367e099f1e5a0c45fdc9afd316d969c9f340
carlosrg <crealgar00@gmail.com>
2022-04-12 23:49:24 +0200

    Sms class and menu buttons.

--- app/src/main/java/com/example/tarea3_programacionmultimedia/MainActivity.java
+++ app/src/main/java/com/example/tarea3_programacionmultimedia/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.tarea3_programacionmultimedia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -15,9 +17,19 @@ import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button Llamada, Sms, Mail, Ajustes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Llamada = findViewById(R.id.BtnLlamada);
+        Sms = findViewById(R.id.BtnSms);
+        Mail = findViewById(R.id.BtnMail);
+        Ajustes = findViewById(R.id.BtnAjustes);
+
+        Sms.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SmsActivity.class);
+            startActivity(intent);
+        });
     }
 }
