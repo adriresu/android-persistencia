5101f9e08000f65f535bc847d60e824b309456ba
PedroReglero <pedropablo@ciclosmontecastelo.com>
2022-05-10 00:19:54 +0200

    Agregada actividad de vibración Cambios en el diseño de la app

--- app/src/main/java/com/example/tarea3_programacionmultimedia/VibracionActivity.java
+++ app/src/main/java/com/example/tarea3_programacionmultimedia/VibracionActivity.java
@@ -8,8 +8,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.widget.Button;
 
 public class VibracionActivity extends AppCompatActivity {
+
+    Vibrator vibratorService;
+
+    Button btnVibracion1;
+    Button btnVibracion2;
+    Button btnVibracion3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +26,56 @@ public class VibracionActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        Vibrator vibratorService = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            vibratorService.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
-        } else {
-            vibratorService.vibrate(500);
-        }
+        vibratorService = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        btnVibracion1 = (Button) findViewById(R.id.btnVibracion1);
+        btnVibracion2 = (Button) findViewById(R.id.btnVibracion2);
+        btnVibracion3 = (Button) findViewById(R.id.btnVibracion3);
+
+
+        btnVibracion1.setOnClickListener(view -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibratorService.vibrate(new long[] {300, 400}, 1);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+
+                }
+                vibratorService.cancel();
+            }
+            else {
+                vibratorService.vibrate(10000);
+            }
+        });
+
+        btnVibracion2.setOnClickListener(view -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibratorService.vibrate(new long[] {200, 300}, 1);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+
+                }
+                vibratorService.cancel();
+            }
+            else {
+                vibratorService.vibrate(9000);
+            }
+        });
+
+        btnVibracion3.setOnClickListener(view -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibratorService.vibrate(new long[] {100, 200}, 1);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+
+                }
+                vibratorService.cancel();
+            }
+            else {
+                vibratorService.vibrate(8000);
+            }
+        });
     }
 }
